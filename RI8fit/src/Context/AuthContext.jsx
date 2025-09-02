4983612257082752b8c009e3fb6d596500df78d7
Akshay <kumarking9848@gmail.com>
2025-09-02 11:20:18 +0530

    eT

--- RI8fit/src/Context/AuthContext.jsx
+++ RI8fit/src/Context/AuthContext.jsx
@@ -51,7 +51,7 @@ export const AuthProvider = ({ children, navigationRef }) => {
       const newAccessToken = json?.data?.access_token;
 
       if (newAccessToken) {
-        const newExpiry = Date.now() + 1 * 60 * 1000;
+        const newExpiry = Date.now() + 15 * 60 * 1000;
         await setItem(ACCESS_TOKEN, newAccessToken);
         await setItem(EXPIRY_TIME, newExpiry.toString());
         await setItem(REFRESH_TOKEN, refresh_Token);
